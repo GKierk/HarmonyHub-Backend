9ebc9bf8fc0ca5510eafbc6573b1b1911e3f9bbc
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-24 10:55:22 +0200

    Finished implementing MusicRecord

--- HarmonyHub-Backend/Models/MusicRecord.cs
+++ HarmonyHub-Backend/Models/MusicRecord.cs
@@ -2,9 +2,9 @@
 
 public class MusicRecord
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
-    public string Artist { get; set; }
+    public string? Artist { get; set; }
 
     public int PublicationYear { get; set; }
     
@@ -20,7 +20,45 @@ public class MusicRecord
     {
         if (string.IsNullOrEmpty(Title))
         {
-
+            throw new ArgumentNullException("Title can't be empty.");
         }
+    }
+
+    public void VerifyArtist()
+    {
+        if (string.IsNullOrEmpty(Artist))
+        {
+            throw new ArgumentNullException("Artist can't be empty.");
+        }
+    }
+
+    public void VerifyPublicationYear()
+    { 
+        if (PublicationYear > DateTime.Now.Year)
+        {
+            throw new ArgumentOutOfRangeException("Publication year can't be after the current year.");
+        }
+
+        if (PublicationYear < 1930)
+        {
+            // First vinyl record was made in 1930.
+            throw new ArgumentOutOfRangeException("The year must be after 1929.");
+        }
+    }
+
+    public void VerifyDuration()
+    {
+        if (Duration < 0)
+        {
+            throw new ArgumentOutOfRangeException("Duration must be greater than 0.");
+        }
+    }
+
+    public void Verify()
+    {
+        VerifyTitle();
+        VerifyArtist();
+        VerifyPublicationYear();
+        VerifyDuration();
     }
 }
