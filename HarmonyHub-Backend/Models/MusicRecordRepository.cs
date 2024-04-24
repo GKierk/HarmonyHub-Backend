ac958d02e3067a0e10eba42db81530e69f59111f
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-24 11:14:10 +0200

    Started implementing MusicRecordRepository

---
+++ HarmonyHub-Backend/Models/MusicRecordRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace HarmonyHub_Backend.Models;
+
+public class MusicRecordRepository
+{
+    public List<MusicRecord> MusicRecords { get; set; } = new List<MusicRecord>();
+
+    public MusicRecord Create(MusicRecord musicRecord)
+    {
+        if (MusicRecords.Count == 0)
+        {
+            musicRecord.Id = 1;
+        }
+        else
+        {
+            musicRecord.Id = MusicRecords.Count + 1;
+        }
+
+        MusicRecords.Add(musicRecord);
+        return musicRecord;
+    }
+
+    public IEnumerable<MusicRecord> Read()
+    {
+        IQueryable<MusicRecord> query = MusicRecords.AsQueryable();
+
+        return query;
+    }
+
+    public MusicRecord? Update(int id, MusicRecord musicRecord)
+    {
+        musicRecord.Id = id;
+        MusicRecord? recordToUpdate = MusicRecords.Find(r => r.Id == musicRecord.Id);
+        if (recordToUpdate == null)
+        {
+            return null;
+        }
+
+        int recordIndeex = musicRecord.Id;
+        MusicRecords[--recordIndeex] = musicRecord;
+
+        return recordToUpdate;
+    }
+}
