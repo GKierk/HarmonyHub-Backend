8b3fbda9ec285b6661679f05f10420d19933c10e
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-04-24 10:23:33 +0200

    Started Implementing MusicRecord

---
+++ HarmonyHub-Backend/Models/MusicRecord.cs
@@ -0,0 +1,26 @@
+﻿namespace HarmonyHub_Backend.Models;
+
+public class MusicRecord
+{
+    public string Title { get; set; }
+
+    public string Artist { get; set; }
+
+    public int PublicationYear { get; set; }
+    
+    // Duration in Seconds
+    public int Duration { get; set;}
+    
+    public override string ToString()
+    {
+        return $"Title: {Title}, Artist: {Artist}, Publication Year: {PublicationYear}, Duration in Seconds: {Duration}";
+    }
+
+    public void VerifyTitle()
+    {
+        if (string.IsNullOrEmpty(Title))
+        {
+
+        }
+    }
+}
