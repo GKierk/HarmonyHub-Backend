b1fb97e23ad41f86bf1faf65195ad7706ba82130
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-30 12:32:32 +0200

    Added MusicRecordRepository as singleton

--- HarmonyHub-Backend/Program.cs
+++ HarmonyHub-Backend/Program.cs
@@ -1,7 +1,9 @@
+using HarmonyHub_Backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<MusicRecordRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
